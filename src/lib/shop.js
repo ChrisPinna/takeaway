ab30c46374061c8f602fc7bc5fc5e5d1bf827ffd
christianpinna <christian.a.pinna@gmail.com>
2022-06-28 21:38:18 +0100

    created test for selectItem method to accept correct vabiables

--- src/lib/shop.js
+++ src/lib/shop.js
@@ -2,6 +2,10 @@ class Shop {
   showMenu() {
     return "1- Burger £5.00\n2- Chicken £9.00\n3- Steak £15.00";
   }
+
+  selectItem() {
+
+  }
 }
 
 module.exports = Shop;
--- src/test/shop.test.js
+++ src/test/shop.test.js
@@ -1,8 +1,19 @@
 const Shop = require('../lib/shop')
 
-describe('Shop Class', () => {
-  test('it returns a list of dishes', () => {
-		const shop = new Shop;
-		expect(shop.showMenu()).toBe("1- Burger £5.00\n2- Chicken £9.00\n3- Steak £15.00")
+describe('showMenu method', () => {
+  test('should returns a menu list', () => {
+    const shop = new Shop;
+    expect(shop.showMenu()).toBe("1- Burger £5.00\n2- Chicken £9.00\n3- Steak £15.00");
   });
 });
+
+describe('selectItem method', () => {
+  test('should take a itemId and quantitie', () => {
+    const shop = new Shop;
+    expect(() => {
+      const shop = new Shop;
+      shop.selectItem(1, 2);
+    }).not.toThrow();
+  });
+});
+
