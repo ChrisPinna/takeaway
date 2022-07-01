e2ad2284631bf215972381513b5175007f2072d1
christianpinna <christian.a.pinna@gmail.com>
2022-07-01 18:08:33 +0100

    passed test to select and show 2 separate items from menu

--- src/spec/feature.test.js
+++ src/spec/feature.test.js
@@ -5,9 +5,18 @@ describe('ordering', () => {
     const shop = new Shop;
     expect(shop.showOrder()).toBe("Your basket is empty");
   });
+
   test('order 1 burger', () => {
     const shop = new Shop;
     shop.selectItem(1, 1);
     expect(shop.showOrder()).toBe("Your order\nBurger x1 £5.99\nTotal: £5.99");
   });
+  
+  test('order 1 burger and & Chicken', () => {
+    const shop = new Shop;
+    shop.selectItem(1, 1);
+    shop.selectItem(2, 1);
+    expect(shop.showOrder()).toBe("Your order\nBurger x1 £5.99\nChicken x1 £9.99\nTotal: £15.98");
+  });
+  
 });
